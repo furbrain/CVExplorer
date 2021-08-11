f2e402280d1ec415182140cfcd39618e5a62bd2b
Phil Underwood <beardydoc@gmail.com>
2021-08-11 19:18:14 +0100

    Initial customisation

--- setup.py
+++ setup.py
@@ -17,33 +17,21 @@ def read(*names, **kwargs):
         return fh.read()
 
 
-# previous approach used to ignored badges in PyPI long description
-# long_description = '{}\n{}'.format(
-#     re.compile(
-#         '^.. start-badges.*^.. end-badges',
-#         re.M | re.S,
-#         ).sub(
-#             '',
-#             read('README.rst'),
-#             ),
-#     re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read(join('docs', 'CHANGELOG.rst')))
-#     )
-
 long_description = '{}\n{}'.format(
     read('README.rst'),
     read(join('docs', 'CHANGELOG.rst')),
     )
 
 setup(
-    name='jmct-sampleproject',
+    name='cvexplorer',
     version='0.8.0',
-    description='A skeleton template for Python projects.',
+    description='A GUI for OpenCV',
     long_description=long_description,
     long_description_content_type='text/x-rst',
     license='MIT License',
-    author='Joao Miguel Correia Teixeira',
-    author_email='joaomcteixeira@gmail.com',
-    url='https://github.com/joaomcteixeira/python-project-skeleton',
+    author='Phil Underwood',
+    author_email='beardydoc@gmail.com',
+    url='https://github.com/furbrain/CVExplorer',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(i))[0] for i in glob("src/*.py")],
@@ -62,14 +50,13 @@ setup(
         'Operating System :: Microsoft',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
         ],
     project_urls={
-        'webpage': 'https://github.com/joaomcteixeira/python-project-skeleton',
-        'Documentation': 'https://python-project-skeleton.readthedocs.io/en/latest/',
-        'Changelog': 'https://github.com/joaomcteixeira/python-project-skeleton/blob/master/docs/CHANGELOG.rst',
-        'Issue Tracker': 'https://github.com/joaomcteixeira/python-project-skeleton/issues',
-        'Discussion Forum': 'https://github.com/joaomcteixeira/python-project-skeleton/discussions',
+        'webpage': 'https://github.com/furbrain/CVExplorer',
+        'Documentation': 'https://cvexplorer.readthedocs.io/en/latest/',
+        'Changelog': 'https://github.com/furbrain/CVExplorer/blob/master/docs/CHANGELOG.rst',
+        'Issue Tracker': 'https://github.com/furbrain/CVExplorer/issues',
+        'Discussion Forum': 'https://github.com/furbrain/CVExplorer/discussions',
         },
     keywords=[
         'ci', 'continuous-integration', 'project-template',
@@ -78,33 +65,14 @@ setup(
         ],
     python_requires='>=3.6, <3.9',
     install_requires=[
-        'matplotlib>=3',
-        # 'click',
-        # eg: 'aspectlib==1.1.1', 'six>=1.7',
         ],
     extras_require={
-        # eg:
-        #   'rst': ['docutils>=0.11'],
-        #   ':python_version=="2.6"': ['argparse'],
         },
     setup_requires=[
-        #   'pytest-runner',
-        #   'setuptools_scm>=3.3.1',
         ],
     entry_points={
         'console_scripts': [
-            'samplecli1= sampleproject.cli_int1:main',
             ]
         #
         },
-    # cmdclass={'build_ext': optional_build_ext},
-    # ext_modules=[
-    #    Extension(
-    #        splitext(relpath(path, 'src').replace(os.sep, '.'))[0],
-    #        sources=[path],
-    #        include_dirs=[dirname(path)]
-    #    )
-    #    for root, _, _ in os.walk('src')
-    #    for path in glob(join(root, '*.c'))
-    # ],
     )
